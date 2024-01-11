7ace4bb504b4d0330eb23624d06fdbad98276131
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2024-01-11 14:48:19 -0800

    config entity

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from source.utility.utility import generate_global_timestamp
-
+from source.entity.config_entity import TrainingPipelineConfig
 
 if __name__ == '__main__':
 
     generate_timestamp = generate_global_timestamp()
+
+    train_pipeline_config = TrainingPipelineConfig()
+    print(train_pipeline_config.__dict__)
---
+++ source/constant/constant_train.py
@@ -0,0 +1,16 @@
+TARGET_COLUMN = "churn"
+TRAIN_PIPELINE_NAME = "training_pipeline"
+FILE_NAME = "training_data.csv"
+ARTIFACT_DIR = "artifact"
+
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+MONGODB_URL_KEY = "mongodb+srv://amrutasharnangat2503:mongo@cluster0.r6fy3lw.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "dataset"
+
+DI_COLLECTION_NAME = "telco-customer-churn"
+DI_DIR_NAME = "data ingestion"
+FEATURE_STORE_DIR_PATH = "feature store"
+DI_INGESTION_DIR = "ingested"
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
